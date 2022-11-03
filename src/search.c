f1784035fc7a0901b1c6c74b15be23d784a24b2b
CKCordes <114151334+CKCordes@users.noreply.github.com>
2022-11-03 13:50:49 +0100

    Implemented code for assignment 9

--- src/search.c
+++ src/search.c
@@ -3,11 +3,17 @@
 #include <stdbool.h>
 
 bool search(int a[], int n, int x) {
+    // Base case 1
     if (n == 0) {
         return false;
-    } else if (n > 0 && a[n - 1] == x) {
+    }
+    // Base case 2
+    else if (n > 0 && a[n - 1] == x) {
         return true;
-    } else if (n > 0 && a[n - 1] != x) {
+    }
+    // Recursive step
+
+    else if (n > 0 && a[n - 1] != x) {
         return search(a, n - 1, x);
     }
 }
