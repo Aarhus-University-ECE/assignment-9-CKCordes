1fa2ddaa3bc473d1471d19b026fd9f9dc19bedd8
CKCordes <114151334+CKCordes@users.noreply.github.com>
2022-11-07 15:14:46 +0100

    Now without recursion

--- src/dfs.c
+++ src/dfs.c
@@ -9,6 +9,36 @@
 #include <stdlib.h>  /* abort */
 
 void DFT(node *root) {
+    // Initialising the stack with rooth and having it point to NULL
+    stack *main = malloc(sizeof(stack));
+    main->next = NULL;
+    main->node = root;
+
+    // Creating a node that has the value of root, note that we actually create
+    // a node, NOT a node pointer. This is to make sure we can free the node
+    // pointers in the stack
+    node curr = *root;
+    // if we stille have items in out stack we cant be finished visiting the
+    // entire tree
+    while (!isEmpty(main)) {
+        // popping the top value in the stack to grab the current node
+        curr = *top(main);
+        main = pop(main);
+        // if we have either a branch or leaf at our current nodes rchild we
+        // push that node to visit it later
+        if (curr.rchild != NULL) {
+            main = push(main, curr.rchild);
+        }
+        // The same with the left side of the current node
+        if (curr.lchild != NULL) {
+            main = push(main, curr.lchild);
+        }
+        // The print the current node
+        print_node(&curr);
+    }
+
+    /*
+    // Recursion
     // If the current is non exisitant "pop" it in the call stack and go back
     if (root == NULL) {
         return;
@@ -18,7 +48,7 @@ void DFT(node *root) {
     // Going all the way to the left leaf
     DFT(root->lchild);
     // Going to the right leaf
-    DFT(root->rchild);
+    DFT(root->rchild);*/
 }
 
 node *make_node(int num, node *left, node *right) {
@@ -69,7 +99,6 @@ void print_tree(node *p, int depth) {
 }
 
 stack *push(stack *topp, node *node) {
-    // Not used in the code
     // Allocating memory for the new element in stack
     stack *element = malloc(sizeof(stack));
     // Assigning the values to the stack element
@@ -80,7 +109,6 @@ stack *push(stack *topp, node *node) {
 }
 
 bool isEmpty(stack *topp) {
-    // not used in the code
     // Lokal triviel
     if (topp == NULL) {
         return true;
@@ -90,7 +118,6 @@ bool isEmpty(stack *topp) {
 }
 
 node *top(stack *topp) {
-    // Not used in the code
     // Lokal triviel
     node *topNode = topp->node;
     return topNode;
@@ -100,12 +127,12 @@ node *top(stack *topp) {
 // element from the stack
 
 stack *pop(stack *topp) {
-    // Not used in the code
     // Lokal triviel
+
     stack *temp = topp;
-    topp = topp->next;
+    stack *newTopp = topp->next;
     free(temp);
-    return topp;
+    return newTopp;
 }
 
 void print_stack(stack *topp) {
